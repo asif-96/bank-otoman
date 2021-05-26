0d7f79ace3cb69f70d16069dd0145c897a066527
asif-96 <shuyeb.dk@gmail.com>
2021-05-26 23:25:13 +0600

    working on js file

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('submitBtn').addEventListener('click', function(){
+    document.querySelector('.main').style.display = "none";
+    document.querySelector('.profile').style.display = "block";
+});
+
+
+// const main_field = document.querySelector('.main');
+// const profile = document.querySelector('.profile');
+
+// function login(){
+//     main_field.style.display = 'none';
+//     profile.style.display = 'block';
+
+
+
